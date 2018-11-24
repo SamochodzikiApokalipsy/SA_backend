f87b0afc786b062446be10905ada689fd45d1d95
Pawlllosss <oczadlypawe@gmail.com>
2018-11-24 11:47:18 +0100

    gradle project and db settings

---
+++ src/main/java/pl/hackathon/hackyeah/samochodziki/SamochodzikiApplication.java
@@ -0,0 +1,12 @@
+package pl.hackathon.hackyeah.samochodziki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamochodzikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamochodzikiApplication.class, args);
+	}
+}
