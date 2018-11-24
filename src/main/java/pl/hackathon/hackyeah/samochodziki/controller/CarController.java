39ca9742695cfee1bece71c47f5f33d7d3b45e55
Pawlllosss <oczadlypawe@gmail.com>
2018-11-24 19:00:21 +0100

    tmp cors fix

--- src/main/java/pl/hackathon/hackyeah/samochodziki/controller/CarController.java
+++ src/main/java/pl/hackathon/hackyeah/samochodziki/controller/CarController.java
@@ -3,6 +3,7 @@ package pl.hackathon.hackyeah.samochodziki.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/getAll")
     public ResponseEntity<?> getAll() {
         List<Car> result = carService.findAll();
@@ -25,6 +27,7 @@ public class CarController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/getAllRelative")
     public ResponseEntity<?> getAllRelative() {
         List<Car.CarRelative> result = carService.getAllRelative();
