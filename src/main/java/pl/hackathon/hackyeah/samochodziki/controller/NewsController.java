f0030d6871f63764433a7882bc0febfed784a3a1
Pawlllosss <oczadlypawe@gmail.com>
2018-11-25 08:02:53 +0100

    Added cyclical updates

--- src/main/java/pl/hackathon/hackyeah/samochodziki/controller/NewsController.java
+++ src/main/java/pl/hackathon/hackyeah/samochodziki/controller/NewsController.java
@@ -2,6 +2,7 @@ package pl.hackathon.hackyeah.samochodziki.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,8 +15,7 @@ public class NewsController {
     @Autowired
     NewsService newsService;
 
-
-//    @Scheduled(fixedRate=30000)
+    @Scheduled(fixedRate=30000)
     @GetMapping("/test")
     @ResponseStatus(value = HttpStatus.OK)
     public void updatePopularity() throws Exception {
